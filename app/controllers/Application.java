b52fbaaf5fc69dc6d2e5fba7624661fe456f4993
Serj Sotnikov <bcegoqpehu@gmail.com>
2014-09-27 20:43:15 +0400

    websocket

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -4,32 +4,34 @@ import play.mvc.Controller;
 
 public class Application extends Controller {
 
-    public static void index() {
-        render();
-    }
+  public static void index(Long firstId, Long secondId) {
+    System.out.println("firstId = [" + firstId + "], secondId = [" + secondId + "]");
+    render(firstId, secondId);
+  }
 
-    public static void registration() {
-        render();
-    }
-
-    public static void login() {
-      render();
-    }
-
-    public static void create_bot() {
+  public static void registration() {
     render();
-    }
+  }
 
-    public static void faq() {
+  public static void login() {
     render();
-    }
+  }
 
-    public static void translation() {
-      render();
-    }
-    public static void main() {
-      render();
-    }
+  public static void create_bot() {
+    render();
+  }
+
+  public static void faq() {
+    render();
+  }
+
+  public static void translation() {
+    render();
+  }
+
+  public static void main() {
+    render();
+  }
 
 
 }
